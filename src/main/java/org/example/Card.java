f4f168144f8be0b045d1abdaf0c4ea107a86e174
Kimberley Antao <kimberley2609@gmail.com>
2024-09-20 10:10:16 +0100

    Initialised class variable

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public class Card {
+                    // heart     diamond    spade     club
+    private final String[] SUIT = {"&#x2661", "&#x2662", "&#x2664", "&#x2667"};
+    private final String[] SYMBOL = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private final int[] VALUE = {2,3,4,5,6,7,8,9,10,11,12,13,14};
+    
 }
