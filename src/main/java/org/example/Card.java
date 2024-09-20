282a0a1d503f14cf41751c6ab411897f365fb503
Kimberley Antao <kimberley2609@gmail.com>
2024-09-20 16:43:35 +0100

    added toString method

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -1,9 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Card {
                     // heart     diamond    spade     club
-    private final String[] SUIT = {"&#x2661", "&#x2662", "&#x2664", "&#x2667"};
+    private final String[] SUIT = {"\u2661", "\u2662", "\u2664", "\u2667"};
     private final String[] SYMBOL = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    private final int[] VALUE = {2,3,4,5,6,7,8,9,10,11,12,13,14};
-    
+    private final int[] VALUE = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "SUIT=" + Arrays.toString(SUIT) +
+                ", SYMBOL=" + Arrays.toString(SYMBOL) +
+                ", VALUE=" + Arrays.toString(VALUE) +
+                '}';
+    }
 }
