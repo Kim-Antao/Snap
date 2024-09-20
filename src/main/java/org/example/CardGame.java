e178f02d6da3a7779cbb4cb8c9e51979b84348fb
Kimberley Antao <kimberley2609@gmail.com>
2024-09-20 16:44:31 +0100

    added skeleton of methods required

--- src/main/java/org/example/CardGame.java
+++ src/main/java/org/example/CardGame.java
@@ -3,14 +3,32 @@ package org.example;
 import java.util.ArrayList;
 
 public class CardGame {
-    private ArrayList<String> deckOfCards = new ArrayList<>();
+    private ArrayList<Card> deckOfCards = new ArrayList<>();
 
     public CardGame(){
-//        deckOfCards.add("1");
-//        deckOfCards.add("3");
+
+    }
+    
+    public String getDeck(){
+        return (deckOfCards.toString());
     }
 
-    public String getDeck(){
-        return deckOfCards.toString();
+    public Card dealCard(){
+        return deckOfCards.get(0);
+    }
+
+    public ArrayList<Card> sortDeckInNumber(){
+       // sort the deck by number 222233334444
+        return deckOfCards;
+    }
+
+    public ArrayList<Card> sortDeckIntoSuits() {
+        // 2,3,4... of heart, 2,3,4.... of diamonds
+         return deckOfCards;
+    }
+
+    public ArrayList<Card> shuffleDeck(){
+        // shuffle into a random order
+        return deckOfCards;
     }
 }
