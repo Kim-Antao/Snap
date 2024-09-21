8b5f012ae3992519bbd267fa0519d50d419f85b5
Kimberley Antao <kimberley2609@gmail.com>
2024-09-21 21:58:06 +0100

    Added a constructor and moved the arrays to cardGame file

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -4,17 +4,35 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Card {
-                    // heart     diamond    spade     club
-    private final String[] SUIT = {"\u2661", "\u2662", "\u2664", "\u2667"};
-    private final String[] SYMBOL = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
-    private final int[] VALUE = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+
+    private String suit ;
+    private String symbol;
+    private int value;
+
+    public Card(String suit, String symbol, int value){
+        this.suit = suit;
+        this.symbol = symbol;
+        this.value = value;
+    }
+
+    public String getSYMBOL() {
+        return symbol;
+    }
+
+    public String getSUIT() {
+        return suit;
+    }
+
+    public int getValue() {
+        return value;
+    }
 
     @Override
     public String toString() {
         return "Card{" +
-                "SUIT=" + Arrays.toString(SUIT) +
-                ", SYMBOL=" + Arrays.toString(SYMBOL) +
-                ", VALUE=" + Arrays.toString(VALUE) +
+                "suit='" + suit + '\'' +
+                ", symbol='" + symbol + '\'' +
+                ", value=" + value +
                 '}';
     }
 }
