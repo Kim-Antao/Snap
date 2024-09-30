53f5b2381c3df5a22798225156318f5583073ef6
Kimberley Antao <kimberley2609@gmail.com>
2024-09-30 11:22:38 +0100

    refactored

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,5 @@
 package org.example;
 
-
-import java.util.Objects;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Snap snapGame = new Snap("Snap");
