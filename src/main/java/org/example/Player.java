b6d0ec0e8ce30d57c24d8061976a8a267fcae44f
Kimberley Antao <kimberley2609@gmail.com>
2024-09-30 11:14:40 +0100

    added fields and methods to player class

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class Player {
+
+    private String name;
+    private String fontColor;
+
+    public Player(String name, String fontColor){
+        this.name = name;
+        this.fontColor = fontColor;
+    }
+
+    public String getName() {;
+        return name.substring(0,1).toUpperCase()+name.substring(1).toLowerCase();
+    }
+
+    public String getFontColor() {
+        return fontColor;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFontColor(String fontColor) {
+        this.fontColor = fontColor;
+    }
 }
